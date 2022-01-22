a60324b731b57db26da094d60f3d3b717ea59af7
nggcasey <60057103+nggcasey@users.noreply.github.com>
2022-01-23 07:37:08 +1100

    Update config.lua

--- config.lua
+++ config.lua
@@ -12,9 +12,32 @@ Config.Locations = {
 }
 
 Config.DebrisLocations = {
-
-    [1] = vector4(371.6, -1612.57, 29.29, 92.69),
-    [2] = vector4(1015.63, -900.98, 30.5, 20.05)
+    [1] = {
+        label = "This is Location 1",
+        coords = {
+            Area = vector3(-2838.8, -376.1, 3.55),
+            Coral = {
+                [1] = {
+                    coords = vector3(-2849.25, -377.58, -40.23),
+                    PickedUp = false
+                },
+                [2] = {
+                    coords = vector3(-2838.43, -363.63, -39.45),
+                    PickedUp = false
+                },
+                [3] = {
+                    coords = vector3(-2887.04, -394.87, -40.91),
+                    PickedUp = false
+                },
+                [4] = {
+                    coords = vector3(-2808.99, -385.56, -39.32),
+                    PickedUp = false
+                },
+            }
+        },
+        DefaultDebris = 4,
+        TotalDebris = 4,
+    },
 }
 
 Config.Vehicles = {
